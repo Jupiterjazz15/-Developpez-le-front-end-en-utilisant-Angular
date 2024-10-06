3b4fbe15023b7a886d7328f4ada78d612b163385
coraliehaller <haller.coralie.15@gmail.com>
2024-10-06 13:47:20 +0200

    Repasse sur le service pour bien le commenter + ajoute la mthd getOlympicsValue() pour refactoriser mon code

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+// j'importe le service HttpClient d'Angular pour faire des requêtes HTTP
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -8,23 +9,43 @@ import { Olympic } from 'src/app/core/models/Olympic';
 @Injectable({
   providedIn: 'root',
 })
+
 export class OlympicService {
 
   private olympicUrl = './assets/mock/olympic.json';
+  // propriété privée qui stocke l'URL du fichier JSON qui contient les données des Jeux Olympiques
+
   private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
-  // propriété avec un BehaviorSubject (un type d'Observable qui permet de stocker la dernière valeur émise). On utilise $ pour indique qu'il s'agit d'un observable. Cette valeur sera soit une instance d'Olympic, soit null si aucune donnée n'est disponible. On initialise cette propriété avec null.
+  // propriété avec un $ pour indiquer qu'il s'agit d'un Observable.
+  // BehaviorSubject : type de données Observable + permet de stocker la dernière valeur émise par celu-ci.
+  // cette valeur sera soit une instance d'Olympic, soit null si aucune donnée n'est disponible.
+  // on initialise cette propriété avec null.
+
+  private olympics: Olympic[] | null = null;
+  // propriété qui stocke les données des JO sous forme de tableau d'instances d'Olympic
 
   constructor(private http: HttpClient) {}
-  //j'injecte le service HttpClient d'Angular, utilisé pour faire des requêtes HTTP
+  // à l'aide d'un constructeur, j'injecte le service d'Angular nommé HttpClient (utilisé pr faire des requêtes HTTP)
 
   loadInitialData() {
+  // méthode pour récupérer les données du fichier JSON
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
-      // utilisation du service http avec une requête GET.
-      // je demande à ce que ma réponse soit sous la forme d'un tableau d'instances d'Olympic.
-      // je fais mon appel vers l'URL qui est le valeur de ma propriété olympicUrl
-      // on utilise l'opérateur pipe de RxJS pour transformer le flux de données.
-      tap((value) => this.olympics$.next(value)),
-      // tap récupère les données de la requête (value) et met à jour la valeur de l'Observable olympics$ avec cette nouvelle donnée.
+      // this.http.get = utilisation du service http avec une requête GET.
+      // <Olympic[]> = je demande à ce que ma réponse soit sous la forme d'un tableau d'instances d'Olympic.
+      // this.olympicUrl = je lui passe l'URL du fichier JSON
+      // .pipe = on utilise l'opérateur pipe de RxJS pr transformer un Observable émis par la méthode http.get() en un nouvel Observable qui a été modifié par les opérateurs tap ou catchError
+
+      tap((value) => {
+        this.olympics = value;
+        // je stocke les données des JO dans la propriété olympics
+        this.olympics$.next(value);
+        }),
+      // on utilise l'opérateur tap de RxJS
+      // cet opérateur prend en argument une fonction lambda dont le paramètre est la valeur émise par l'Observable
+      // cad un tableau d'instances d'Olympic
+      // cette fonction sera exécutée à chaque fois que l'Observable émettra
+      // 1 : on intercepte chaque valeur émise par l'Observable 2 : on exécute la fonction fournie 3 : on renvoie la même valeur sans la modifier.
+
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
@@ -32,17 +53,22 @@ export class OlympicService {
         this.olympics$.next(null);
         return caught;
       })
+
     );
   }
 
   getOlympics() {
+  // méthode pr obtenir un Observable avec toutes les données des JO
     return this.olympics$.asObservable();
+    // méthd de la classe BehaviorSubject pr obtenir un Observable à partir d'un BehaviorSubject
   }
 
   getOlympicByName(name: string): Olympic | undefined {
-    const olympics = this.olympics$.getValue(); // Obtenez la valeur actuelle
+  // mthd pour récupérer un objet Olympic par son nom
+    const olympics = this.getOlympicsValue();
 
     if (!olympics) {
+    // on vérifie si la valeur d'olympics est "falsy" cad false, O,nul etc.
       throw new Error('No olympics data available!');
     }
 
@@ -55,18 +81,29 @@ export class OlympicService {
   }
 
   countOlympics(): number {
-    const olympics = this.olympics$.getValue();
-    // constante pour stocker la valeur actuelle de l'Observable olympics avec utilisation de getValue() qui est une méthode de la classe de la classe BehaviorSubject
+  // méthode pr compter le nbr de pays participants aux JO
+    const olympics = this.getOlympicsValue();
+
     return olympics ? olympics.length : 0;
     // opérateur ternaire : si olympics n'est pas null, on retourne la longueur du tableau, sinon on retourne 0
   }
-  countParticipations(): number {
-    const olympics = this.olympics$.getValue();
 
-    if (olympics && olympics.length > 0) {
+  countParticipations(): number {
+    const olympics = this.getOlympicsValue();
+
+    if (olympics) {
         return olympics[0].participations ? olympics[0].participations.length : 0;
     }
     return 0;
-}
+  }
+
+  private getOlympicsValue(): Olympic[] {
+  //je fais une mthd prive pour obtenir les données des JO car j'en ai besoin dans bcp de mes mthds
+    const olympics = this.olympics$.getValue();
+    if (!olympics) {
+      throw new Error('No olympics data available!');
+    }
+    return olympics;
+  }
 
 }
